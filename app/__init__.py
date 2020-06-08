4859f4c1a8a5f7aac537595edb9bd3a1c6169e27
Lee-Junhee <junhee.lee002@gmail.com>
2020-06-07 21:43:59 -0400

    implemented login features

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for
 import sqlite3
 app = Flask(__name__)
+
+def execute(cmd, args):
+    #TO-DO: Write fxn to execute the database commands and return any results
+    pass
 
 @app.route("/")
 def home():
@@ -10,9 +14,40 @@ def home():
 def sign_in():
     return render_template("signin.html")
 
+@app.route('verify')
+def verify():
+    username = request.form('username')
+    cmd = 'SELECT password FROM users WHERE username=?'
+    password = execute(cmd, (username))
+    if password == request.form['password']:
+        cmd = 'SELECT id FROM users WHERE username=?'
+        session['uid'] = execute(cmd, (username))
+        flash('Successfully logged in.', 'success')
+        return redirect(url_for('homepage'))
+    flash('Incorrect username/password combination!', 'error')
+    return redirect(url_for('sign_in'))
+
 @app.route("/sign_up")
 def sign_up():
     return render_template("signup.html")
+
+@app.route("/create", methods=['POST'])
+def create_account():
+    username = request.form('username')
+    cmd = 'SELECT password FROM users WHERE username=?'
+    if execute(cmd, (username)):
+        flash('Username is taken!', 'error')
+        return redirect(url_for('sign_up'))
+    password = request.form['password']
+    if password != request.form['verify']:
+        flash('Passwords do not match!', 'error')
+        return redirect(url_for('sign_up'))
+    cmd = 'INSERT INTO users VALUES (?, ?, ?)'
+    execute(cmd, (username, password, []))
+    cmd = 'SELECT id FROM users WHERE username=?'
+    session['uid'] = execute(cmd, (username))
+    flash('Account successfully created.', 'success')
+    return redirect(url_for('homepage'))
 
 @app.route("/homepage")
 def homepage():
