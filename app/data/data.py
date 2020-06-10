a3c149fa020c66bc47aea3b8a84a2487a178ffe1
Manfred <mtan00@stuy.edu>
2020-06-10 03:25:36 -0400

    added get and change values in database

--- app/data/data.py
+++ app/data/data.py
@@ -17,6 +17,10 @@ def createClothing():
     return None
 
 def addUsers(username, password, weights):
+    """
+    adds new user, automatically gives them a unique user_id
+    returns user_id
+    """
     command = "SELECT user_id FROM users;"
     c.execute(command)
     q = c.fetchall()
@@ -24,25 +28,71 @@ def addUsers(username, password, weights):
     print(new_id)
     command = "INSERT INTO users VALUES({},\"{}\",\"{}\", {})".format(new_id, username, password, weights)
     c.execute(command)
-    print(command)
-    return None
+    print(new_id)
+    return(new_id)
 
 
 def addClothing(user_id, name, picture):
+    """
+    adds a new clothing item. returns clothing_id
+    """
     command = "SELECT clothing_id FROM clothing;"
     c.execute(command)
     q = c.fetchall()
     new_id = q[-1][0] + 1
     print(new_id)
     command = "INSERT INTO clothing VALUES({},{},\"{}\",\"{}\")".format(new_id, user_id, name, picture)
-    c.execute(command)
-    return None
+    c.execute(new_id)
+    return(new_id)
 
+def getWeight(user_id):
+    """
+    returns weight given user_id
+    """
+    command = "SELECT weights FROM users WHERE user_id = " + str(user_id)
+    c.execute(command)
+    result = c.fetchall()
+    weight = result[0][0]
+    print(weight)
+    return(weight)
+
+def getAllClothing(user_id):
+    """
+    returns array of (item,link) for each item with user_id
+    """
+    command = "SELECT name,picture FROM clothing WHERE user_id = {}".format(user_id)
+    c.execute(command)
+    result = c.fetchall()
+    print(result)
+    return(result)
+
+def getClothing(user_id, clothing_id):
+    """
+    returns specific item given user_id and clothing_id
+    """
+    command = "SELECT name FROM clothing WHERE user_id = {} AND clothing_id = {}".format(user_id, clothing_id)
+    c.execute(command)
+    result = c.fetchall()
+    clothing = result[0][0]
+    print(clothing)
+    return(clothing)
+
+def changeWeight(user_id, weight):
+    """
+    changes the weight of user given user_id
+    """
+    command = "UPDATE users SET weights = \"{}\" WHERE user_id = {};".format(weight, user_id)
+    c.execute(command)
+    return(weight)
 
 #addUsers('three', 'three', '30.0')
 #addUsers(2, 'aaa', 'bbb', '14.0')
-createClothing()
-addClothing(0, 'jacket', 'example.com')
+#createClothing()
+#addClothing(0, 'jacket', 'example.com')
+#getWeight(2)
+#getClothing(0, 1)
+#getAllClothing(0)
+#changeWeight(2, 13.0)
 
 db.commit() #save changes
 db.close()  #close database
