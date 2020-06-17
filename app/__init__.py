e89ac32588efce312f51fa4938cbc07bc493a86c
David - Mongo Virtual Box <dxiedeng00@stuy.edu>
2020-06-16 20:14:08 -0400

    Uncommented another thing in init, added button for log out

--- app/__init__.py
+++ app/__init__.py
@@ -125,7 +125,7 @@ def update_weights():
 def settings():
     clothes = getAllClothing(session['uid'])
     print(clothes)
-    return render_template("settings.html") #, clothes=clothes)
+    return render_template("settings.html"), clothes=clothes)
 
 @app.route("/remove_clothing", methods=['POST'])
 def remove_clothing():
